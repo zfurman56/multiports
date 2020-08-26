0da0b4725459b7915e784b805a2b52672fb98691
Zach Furman <zach.furman1@gmail.com>
2020-08-25 18:52:46 -0700

    Reorganized code

---
+++ common/network.py
@@ -0,0 +1,48 @@
+import numpy as np
+import networkx as nx
+from copy import deepcopy
+from itertools import product
+
+
+class Node:
+    def __init__(self, scatter_matrix):
+        self._scatter_matrix = scatter_matrix
+        self.metadata = {}
+
+    def _scatter(self, inputs):
+        return self._scatter_matrix.dot(inputs)
+
+    def _to_inputs(self, input_edges):
+        return [e['amplitude'] for e in sorted(input_edges, key=lambda x: x['in_order'])]
+
+    def _to_outputs(self, outputs, output_edges):
+        for i, edge in enumerate(sorted(output_edges, key=lambda x: x['out_order'])):
+            edge['amplitude'] = outputs[i]
+
+    def update(self, input_edges, output_edges):
+        inputs = self._to_inputs(input_edges)
+        self._to_outputs(self._scatter(inputs), output_edges)
+
+class Detector(Node):
+    def __init__(self, scatter_matrix):
+        super().__init__(scatter_matrix)
+        self.amplitudes = np.zeros(np.shape(scatter_matrix)[1])
+
+    def _scatter(self, inputs):
+        self.amplitudes += inputs
+        return super()._scatter(inputs)
+        
+class ScatteringNetwork:
+    def __init__(self):
+        self.network = nx.DiGraph()
+
+    def step(self):
+        # Create temp copy network to modify
+        network_mut = deepcopy(self.network)
+        nx.set_edge_attributes(network_mut, 0, 'amplitude')
+
+        for rnode, wnode in zip(self.network.nodes, network_mut.nodes):
+            wnode.update([self.network[a][b] for a,b in self.network.in_edges(rnode)], [network_mut[a][b] for a,b in network_mut.out_edges(wnode)])
+
+        self.network = network_mut
+
--- demos/2Dbarrier.py
+++ demos/2Dbarrier.py
@@ -1,54 +1,14 @@
-import numpy as np
-import networkx as nx
-from copy import deepcopy
-from itertools import product
 from scipy.stats import unitary_group
 import colorsys
-import multiport
+import numpy as np
 import matplotlib.pyplot as plt
+import networkx as nx
 
+# Import common files
+import sys
+sys.path.append('../common')
+from network import ScatteringNetwork, Node
 
-class Node:
-    def __init__(self, scatter_matrix):
-        self._scatter_matrix = scatter_matrix
-        self.metadata = {}
-
-    def _scatter(self, inputs):
-        return self._scatter_matrix.dot(inputs)
-
-    def _to_inputs(self, input_edges):
-        return [e['amplitude'] for e in sorted(input_edges, key=lambda x: x['in_order'])]
-
-    def _to_outputs(self, outputs, output_edges):
-        for i, edge in enumerate(sorted(output_edges, key=lambda x: x['out_order'])):
-            edge['amplitude'] = outputs[i]
-
-    def update(self, input_edges, output_edges):
-        inputs = self._to_inputs(input_edges)
-        self._to_outputs(self._scatter(inputs), output_edges)
-
-class Detector(Node):
-    def __init__(self, scatter_matrix):
-        super().__init__(scatter_matrix)
-        self.amplitudes = np.zeros(np.shape(scatter_matrix)[1])
-
-    def _scatter(self, inputs):
-        self.amplitudes += inputs
-        return super()._scatter(inputs)
-        
-class ScatteringNetwork:
-    def __init__(self):
-        self.network = nx.DiGraph()
-
-    def step(self):
-        # Create temp copy network to modify
-        network_mut = deepcopy(self.network)
-        nx.set_edge_attributes(network_mut, 0, 'amplitude')
-
-        for rnode, wnode in zip(self.network.nodes, network_mut.nodes):
-            wnode.update([self.network[a][b] for a,b in self.network.in_edges(rnode)], [network_mut[a][b] for a,b in network_mut.out_edges(wnode)])
-
-        self.network = network_mut
 
 # Encode a complex number x as an RGB tuple for visualization
 def complex_to_rgb(x):
@@ -65,13 +25,12 @@ def draw(lattice):
 def onclick(event):
     plt.clf()
     lat.step()
-    lat.step()
     draw(lat)
     plt.draw()
 
 
 lat = ScatteringNetwork()
-np.random.seed(2)
+np.random.seed(0)
 multiport_matrix1 = unitary_group.rvs(4)
 multiport_matrix2 = unitary_group.rvs(4)
 mirror_matrix = np.array([[1]])
@@ -108,9 +67,8 @@ for i in range(0, 10):
 
 nx.set_edge_attributes(lat.network, 0, 'amplitude')
 
-lat.network[nodes[5][4]][nodes[5][3]]['amplitude'] = 1j
+lat.network[nodes[7][4]][nodes[7][3]]['amplitude'] = 1j
 
 draw(lat)
 plt.gcf().canvas.mpl_connect('button_press_event', onclick)
 plt.show()
-
